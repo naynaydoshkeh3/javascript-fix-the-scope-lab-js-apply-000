1ee14cd2b6e6b8585eb4ce728ecb8b9201c672cb
nadine ebrani <nadinedoshkeh3@gmail.com>
2018-06-04 00:20:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ function yourAnimal() {
   return yourAnimal
 }
 
-
+ const two = 2
 function add2(n) {
   return n + two;
 
   // Feel free to move things around!
-  const two = 2
+ // const two = 2
 }
 
 var funkyFunction = function() {
